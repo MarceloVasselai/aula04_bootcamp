a0470ff7d85a3a3b65c288cd84ea119e3e4912d7
Marcelo Vasselai <mvasselai@gmail.com>
2025-03-07 18:17:04 -0300

    Exercícios 4 - final

--- exercicios.py
+++ exercicios.py
@@ -207,6 +207,7 @@ print(estoque_positivo)
 # 14. Extração de Chaves e Valores
 # Objetivo: Dado um dicionário, criar listas separadas para suas chaves e valores.
 
+
 dicionario = {"a": 1, "b": 2, "c": 3}
 chaves = list(dicionario.keys())
 valores = list(dicionario.values())
