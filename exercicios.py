066a6334782e201dec4e269e6dcfa8b7a746001d
Marcelo Vasselai <mvasselai@gmail.com>
2025-03-10 16:10:26 -0300

    lição 4

--- exercicios.py
+++ exercicios.py
@@ -198,6 +198,7 @@ print(dicionario_fundido)
 # 13. Filtragem de Dados em Dicionário
 # Objetivo: Dado um dicionário de estoque de produtos, filtrar aqueles com quantidade maior que 0.
 
+
 estoque = {"Teclado": 10, "Mouse": 0, "Monitor": 3, "CPU": 0}
 
 estoque_positivo = {produto: quantidade for produto, quantidade in estoque.items() if quantidade > 0}
@@ -206,7 +207,6 @@ print(estoque_positivo)
 
 # 14. Extração de Chaves e Valores
 # Objetivo: Dado um dicionário, criar listas separadas para suas chaves e valores.
-
 
 dicionario = {"a": 1, "b": 2, "c": 3}
 chaves = list(dicionario.keys())
