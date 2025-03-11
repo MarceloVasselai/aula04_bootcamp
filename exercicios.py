e4941c22f4f15cd98fb493684985538ef1e685c6
Marcelo Vasselai <mvasselai@gmail.com>
2025-03-11 09:39:47 -0300

    Teste

--- exercicios.py
+++ exercicios.py
@@ -37,11 +37,11 @@ bonus_valido: bool = False
 
 while not nome_valido:
     try:
-        nome = input("Digite seu nome : ")
+        nome = input("Digite seu nome: ")
 
         # Verifica se o nome está vazio
         if len(nome) == 0:
-            raise ValueError("O nome não pode estar vazio. ")
+            raise ValueError("O nome não pode estar vazio.. ")
         # Verifica se há números no nome
         elif any(char.isdigit() for char in nome):
             raise ValueError("O nome não deve conter números...")
